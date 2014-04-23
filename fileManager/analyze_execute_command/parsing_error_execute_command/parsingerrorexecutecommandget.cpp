d71002fdc9a5c0df60a84e2c90af254f9d4a3351
Alexcei88 <Lex9@bk.ru>
2014-04-23 21:55:18 +0400

    Механизм решения ошибок: в мепе храним лямбды функции, которые будут вызыватся когда будем решать проблему

--- fileManager/analyze_execute_command/parsing_error_execute_command/parsingerrorexecutecommandget.cpp
+++ fileManager/analyze_execute_command/parsing_error_execute_command/parsingerrorexecutecommandget.cpp
@@ -15,5 +15,12 @@ ParsingErrorExecuteCommandGet::~ParsingErrorExecuteCommandGet()
 void ParsingErrorExecuteCommandGet::FillErrorIdDescription()
 {   
     errorIdDescription[ErrorType::GET_UNAVAIBLE_CONTENT] = "not avaible";
+
+    auto callbackSolution = [&](const QString& file)->void {
+        std::cout<<"1"<<std::endl;
+
+    };
+    funcErrorSolution[ErrorType::GET_UNAVAIBLE_CONTENT] = callbackSolution;
+
 }
 //----------------------------------------------------------------------------------------/
